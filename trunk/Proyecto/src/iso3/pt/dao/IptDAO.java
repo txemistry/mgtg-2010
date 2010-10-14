dc192158db741c6a80d18530b0f8515de375ba23
jose.gg88 <jose.gg88@8e7d31c8-a057-9e3e-5a0a-bc550b56ffa6>
2010-10-14 19:49:02 +0000

    getEvaluaciones hecho de una forma un poco rara y hecha getProfesor

--- trunk/Proyecto/src/iso3/pt/dao/IptDAO.java
+++ trunk/Proyecto/src/iso3/pt/dao/IptDAO.java
@@ -1,6 +1,12 @@
 package iso3.pt.dao;
 
+import iso3.pt.model.Evaluacion;
+import iso3.pt.model.Profesor;
+
+import java.util.Set;
+
 public interface IptDAO 
 {
-
+	public Profesor getProfesor(int idAsignatura);
+	public Set<Evaluacion> getEvaluaciones(int idAsignatura, int idAlumno);
 }
