806cbe575cc2d83356b2f4596b21532af464a761
jose.gg88 <jose.gg88@8e7d31c8-a057-9e3e-5a0a-bc550b56ffa6>
2010-10-18 15:49:16 +0000

    La caché esta bien hecha y funciona

--- trunk/Proyecto/src/iso3/pt/dao/pruebaDAO.java
+++ trunk/Proyecto/src/iso3/pt/dao/pruebaDAO.java
@@ -2,41 +2,54 @@ package iso3.pt.dao;
 
 import iso3.pt.model.Alumno;
 import iso3.pt.model.Asignatura;
-import iso3.pt.model.Evaluacion;
 import iso3.pt.model.Profesor;
+import iso3.pt.model.Unidad;
 
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
-public class pruebaDAO {
-
-
-	@SuppressWarnings("rawtypes")
+public class pruebaDAO 
+{
+	
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) 
 	{
 		ptDAO dao = ptDAO.getInstancia();
 		
-		Map<Integer, Asignatura> mapa = dao.getCache();
+		Map<Integer, Asignatura> cache = dao.getCache();
 		
 		//Prueba de la cache
 		System.out.println("Prueba de la Cache");
-		Iterator it = mapa.entrySet().iterator();
+		Iterator it = cache.entrySet().iterator();
 		while(it.hasNext()) 
 		{
 			Map.Entry ent = (Map.Entry)it.next();
 			Asignatura asig = (Asignatura)ent.getValue();
-			//System.out.println(asig.toString());
-			System.out.println(asig.getProfesor().toString());
-			//Set<Alumno> alumnos = asig.getAlumnos();
-			//for(Alumno al:alumnos)
-				//System.out.println(al.toString());
+			System.out.println("Datos basicos de la Asignatura");
+			System.out.println("        " + asig.toString());
+			Profesor prof = asig.getProfesor();
+			System.out.println("        Datos del profesor       " + prof.toString() );
+			Set<Alumno> alumnos = asig.getAlumnos();
+			System.out.println("        Datos de los alumnos");
+			for(Alumno al:alumnos)
+			{
+				System.out.println("               " + al.toString());
+			}
+			Set<Unidad> unidades = asig.getUnidades();
+			System.out.println("        Datos de las unidades");
+			for(Unidad uni:unidades)
+			{
+				System.out.println("               " + uni.toString());
+			}
 		}
 		System.out.println();
 		System.out.println();
 		
+		dao.close();
 		
-		//Prueba de getProfesor
+		
+		/*//Prueba de getProfesor
 		System.out.println("Prueba de getProfesor");
 		Profesor profesor = dao.getProfesor(1);
 		System.out.println(profesor.toString());
@@ -55,7 +68,10 @@ public class pruebaDAO {
 		System.out.println("Prueba de getAlumnos");
 		Set<Alumno> alumnos = dao.getAlumnos(1);
 		for(Alumno al:alumnos)
-			System.out.println(al.getNombre());
+			System.out.println(al.getNombre());*/
+		
+		
+		
 	}
 
 }
